8f78fda8943316a1ba15ec39e969bde741983f90
kornilov vadim <vkornilov@studio-tg.ru>
2024-04-01 16:08:09 +0300

    11

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,8 @@
-import { Suspense, useState } from "react";
-import { Link, Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./styles/index.scss";
 import { classNames } from "shared/lib/classNames";
 import { UseTheme } from "app/providers/ThemeProvider";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
+import { AppRouter } from "./providers/router";
 
 const App = () => {
   const { theme, toggleTheme } = UseTheme();
@@ -13,12 +11,7 @@ const App = () => {
       <button onClick={toggleTheme}>Toggle theme</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={"/about"} element={<AboutPage />} />
-          <Route path={"/"} element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
