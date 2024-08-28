784660c03cb28b4f45f90fb6294b4d710db2e29f
Kornilov Vadim <vandam90@yandex.ru>
2024-08-28 20:25:09 +0300

    10

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
 import { Link, Route, Routes } from "react-router-dom";
 import "./styles/index.scss";
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { Suspense, useContext, useState } from "react";
-
-import { useTheme } from "./theme/useTheme";
-import { classNames } from "./helpers/classNames/classNames";
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from "app/providers/ThemeProvider";
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -17,8 +16,8 @@ export const App = () => {
       <Link to={"/about"}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path={"/about"} element={<AboutPageAsync />} />
-          <Route path={" /"} element={<MainPageAsync />} />
+          <Route path={"/about"} element={<AboutPage />} />
+          <Route path={" /"} element={<MainPage />} />
         </Routes>
       </Suspense>
     </div>
